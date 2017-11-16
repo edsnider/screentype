46496312548737cfa87e684d925540cbca19eaf6
Ed Snider <ed.snider@infernored.com>
2017-11-15 19:09:08 -0500

    updated AssemblyInfo

--- src/ScreenType/Properties/AssemblyInfo.cs
+++ src/ScreenType/Properties/AssemblyInfo.cs
@@ -3,4 +3,6 @@
 [assembly: AssemblyTitle("ScreenType")]
 [assembly: AssemblyProduct("ScreenType")]
 [assembly: AssemblyCopyright("2017 Ed Snider")]
-[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
